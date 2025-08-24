516656491892269d855d7b9aa1cef5fcbed66298
Debasish Barai <debasishbaraiju@gmail.com>
2025-08-25 02:24:10 +0530

    Update login/page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
 import { LoginForm } from '@/components/LoginForm';
-import { SignInForm } from '@/components/SignInForm';
 
 export default function LoginPage() {
   return (
